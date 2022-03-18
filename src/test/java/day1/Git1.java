4488bffb8f202ecb15f0f0c1d93b5745cd565cb9
mfirez <mehmetfirez@hotmail.com>
2022-03-18 14:49:57 +0100

    Git1 and GitNotlar Files Updated

--- src/test/java/day1/Git1.java
+++ src/test/java/day1/Git1.java
@@ -4,6 +4,7 @@ public class Git1 {
     public static void main(String[] args) {
 
         System.out.println("yeni method eklendi");
+        System.out.println("-----");
 
     }
 }
--- src/test/java/day1/GitNotlar.java
+++ src/test/java/day1/GitNotlar.java
@@ -11,5 +11,24 @@ public class GitNotlar {
      * git add . ---> kirmizi olan bütün class lari dosyalari ekler. gitignore eklediklerimiz haric
      * git commit -m"All Project Files Uploaded(yazmak istediginiz mesaj)" ---> dosyamizi kaydetmis oluruz
      *
+     * GIT BASE KOMUT OZETI
+     *
+     * ->git init -- ilk kez projemize GIT i tanimlamak icin kullanilir, tek seferlik calistirilir.
+     * .gitignore   -->  ozel bir file dir. git in track sistemine dahil etmek istemediginiz dosya ve/veya  klasorler bu file icin de satir satir tutulur.
+     * Sadece LOCAL de calisma yapacak iseniz alttaki mail ve isim degerleri eklenmeli!!!
+     * git config --global user.email "you@example.com"
+     * git config --global user.name "Your Name"
+     * ->git add dosyaIsmi.uzantisi  -- untrackt file lara kancayi takmak icin / tracked yapmak icin kullanilir
+     * ->git add . -- butun untracked dosylari EKLEMEK icin / yani tracked yapmak icin kullanilir!
+     * ->git commit -m"commit mesaji yaz"  -- eklenen ve tracked olan dosyalari KAYDETMEK ICIN (Snapshot!!)
+     *                                            peki nereye -> Local git directory e! LOCAL REPO!
+     * ->git status -- untracked , tracked ve modifed dosyalari bize getirir gosterir
+     * ->git log -- commit gecmisini gosterir
+     * ->git push -- localdeki degisikleri remote repoya gonderir !!
+     * ->git pull -- remotedaki degisiklikleri local repo alir!!
+     * ->git branch -- mevcut branchlari listeler
+     * ->git branch aaa  -- aaa adinda yeni branch olusturur ve bu branch main branch in 1 e 1 clone u olur!!
+     * ->git checkout aaa  --  aaa branch ina gecis yapar!!
+     *
      */
 }
